60f055776f9b908273416ed329a8f723eb52fd1d
Lorenzothe <140451743+Lorenzothe@users.noreply.github.com>
2024-10-18 09:05:30 -0300

    a

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function criaCart (Categoria, Pergunta, Resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
-    cartao.className = "cartao";
+    cartao.className = 'cartao';
     cartao.innerHTML = `   
 <div class="Cont-cartão">
     <h3>${Categoria}</h3>
