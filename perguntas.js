906b3a6448620b240418c07081a5d7fcdf5e9fd1
Lorenzothe <140451743+Lorenzothe@users.noreply.github.com>
2024-10-18 08:40:29 -0300

    \

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criaCart(
+    'matematica',
+    'quanto é 0 elevado 0?',
+    'indefinido'
+)
+criaCart(
+    'biologia',
+    'quais os sangues existentes do sistema ABO',
+    'A, B, AB, O'
+)
+criaCart(
+    'historia',
+    'quando começou e terminou a ditadura',
+    '1964-1985'
+)
+criaCart(
+    'Biologia',
+    'qual a media de tamanho de pênis no Brasil',
+    '14,2'
+)
+criaCart(
+    'robotica',
+    'o que significa o pino VCC',
+    ' fonte de alimentação do coletor do transistor'
+)
+criaCart(
+    'historia',
+    'quem matou hitler?',
+    'hitler'
+)
+criaCart(
+    'química',
+    'o que é alcano',
+    'um hidrocarboneto aberto com ligação simples'
+)
+criaCart(
+    'geografia',
+    'qual capital do peru',
+    'Lima'
+)
+criaCart(
+    'historia',
+    'quem foi george washington',
+    'foi o primeiro presidente dos USA'
+)
+criaCart(
+    'sociologia',
+    'o que karl marks fez',
+    'o comunismo'
+)
+criaCart(
+    'matematica',
+    'qual é o melhor número?',
+    '69'
+)
+criaCart(
+    'matematica',
+    'quem descobriu o teorema de piatagoras',
+    'pitagoras'
+)
+criaCart(
+    'matematica',
+    'cosceno é x ou y?',
+    'x'
+)
+criaCart(
+    'quimica',
+    'o que é benzeno?',
+    'uma cadeia de carbono fechada com ligações duplas intercaladas'
+)
+criaCart(
+    'quimica',
+    'como identificar fenol?',
+    'analisando se contém um grupo hidroxila ligado a um anél aromático'
+)
+criaCart(
+    'história',
+    'quem é a mãe de pitagoras',
+    'Pythais'
+)
+criaCart(
+    'geografia',
+    'onde fic ao brasil',
+    'fica na américa do sul'
+)
+criaCart(
+    '',
+    'pergunta',
+    'resposta'
+)
+criaCart(
+    'historia',
+    'pergunta',
+    'resposta'
+)
+criaCart(
+    'historia',
+    'pergunta',
+    'resposta'
+)
