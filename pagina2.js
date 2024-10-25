9ad430dfd2a88854030bfe8d275029190857470a
Lorenzothe <140451743+Lorenzothe@users.noreply.github.com>
2024-10-25 11:51:48 -0300

    a

---
+++ pagina2.js
@@ -0,0 +1,17 @@
+function criaCart (Categoria, Pergunta, Resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `   
+<div class="cont-Cartao">
+    <h3>${Categoria}</h3>
+    <div class="pergunta-cartao">
+        <p>${Pergunta}</p>
+    </div>
+    <div class="resposta-cartao">
+        <p>${Resposta}</p>
+    </div>
+</div>
+    `
+    container.appendChild(cartao);
+}
