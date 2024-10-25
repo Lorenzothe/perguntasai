ec7a8d8ebaed998f40e73482d56907e57bece1fd
Lorenzothe <140451743+Lorenzothe@users.noreply.github.com>
2024-10-25 12:22:53 -0300

    a

--- pagina2.js
+++ pagina2.js
@@ -3,7 +3,7 @@ function criaCart (Categoria, Pergunta, Resposta) {
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `   
-<div class="cont-Cartao">
+<div class="cont-Cartao" onclick="mostrarResp(this)">
     <h3>${Categoria}</h3>
     <div class="pergunta-cartao">
         <p>${Pergunta}</p>
@@ -17,4 +17,11 @@ function criaCart (Categoria, Pergunta, Resposta) {
 }
 function mostrarResp(card) {
     const Resposta = card.querySelector('.resposta-cartao');
+    if(Resposta.style.display === 'none'){
+        Resposta.style.display = 'block'
+
+    }
+    else{
+        Resposta.style.display = 'none'
+    }
 }
