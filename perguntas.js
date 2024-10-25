d9c4fbb7680998c2d94b6ce7a54d1425dc32c889
Lorenzothe <140451743+Lorenzothe@users.noreply.github.com>
2024-10-25 11:30:41 -0300

    A

--- perguntas.js
+++ perguntas.js
@@ -1,90 +1,90 @@
 criaCart(
     'Matematica',
-    'quanto é 0 elevado 0?',
-    'indefinido'
+    'Quanto é 0 elevado 0?',
+    'Indefinido'
 )
 criaCart(
     'Biologia',
-    'quais os sangues existentes do sistema ABO',
+    'Quais os sangues existentes do sistema ABO',
     'A, B, AB, O'
 )
 criaCart(
     'Historia',
-    'quando começou e terminou a ditadura',
+    'Quando começou e terminou a ditadura',
     '1964-1985'
 )
 criaCart(
     'Empreendedorismo',
-    'quem é o demônio na Terra?',
+    'Quem é o demônio na Terra?',
     'Carmen'
 )
 criaCart(
     'Robotica',
-    'o que significa o pino VCC',
-    ' fonte de alimentação do coletor do transistor'
+    'O que significa o pino VCC',
+    'É a fonte de alimentação do coletor do transistor'
 )
 criaCart(
     'Historia',
-    'quem matou Getulio Vargas?',
+    'Quem matou Getulio Vargas?',
     'Getulio Vargas'
 )
 criaCart(
     'Química',
-    'o que é alcano',
-    'um hidrocarboneto aberto com ligação simples'
+    'O que é alcano',
+    'Um hidrocarboneto aberto com ligação simples'
 )
 criaCart(
     'Geografia',
-    'qual capital do peru',
+    'Qual capital do peru',
     'Lima'
 )
 criaCart(
     'Historia',
-    'quem foi george washington',
-    'foi o primeiro presidente dos USA'
+    'Quem foi george washington',
+    'Foi o primeiro presidente dos USA'
 )
 criaCart(
     'Sociologia',
-    'o que karl marks fez',
-    'falou sobre o comunismo'
+    'O que karl marks fez',
+    'Falou sobre o comunismo'
 )
 criaCart(
     'Matematica',
-    'se joãozinho fumou 5 maconhas com a viviane, qual a massa do sol?',
+    'Se joãozinho fumou 5 maconhas com a viviane, qual a massa do sol?',
     '3,147249 homens pobres, a bicicleta esta a 217km/h e o sol esta a 4394km da Terra'
 )
 criaCart(
     'Matematica',
-    'quem descobriu o teorema de piatagoras',
-    'pitagoras'
+    'Quem descobriu o teorema de piatagoras',
+    'Pitagoras'
 )
 criaCart(
     'Matematica',
-    'cosceno é x ou y?',
-    'x'
+    'Cosceno é x ou y?',
+    'X'
 )
 criaCart(
     'Quimica',
-    'o que é benzeno?',
-    'uma cadeia de carbono fechada com ligações duplas intercaladas'
+    'O que é benzeno?',
+    'Uma cadeia de carbono fechada com ligações duplas intercaladas'
 )
 criaCart(
     'Quimica',
-    'como identificar fenol?',
-    'analisando se contém um grupo hidroxila ligado a um anél aromático'
+    'Como identificar fenol?',
+    'Analisando se contém um grupo hidroxila ligado a um anél aromático'
 )
 criaCart(
     'História',
-    'quem é a mãe de pitagoras',
+    'Quem é a mãe de pitagoras',
     'Pythais'
 )
 criaCart(
     'Geografia',
-    'onde fica o brasil',
-    'fica na américa do sul'
+    'Onde fica o brasil',
+    'Fica na américa do sul'
 )
 criaCart(
     'Geografia',
-    'onde fica a Coreia do Norte',
-    'fica ao sul da Coreia do Sul'
+    'Onde fica a Coreia do Norte',
+    'Fica ao sul da Coreia do Sul'
 )
